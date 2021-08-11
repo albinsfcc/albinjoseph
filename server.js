1ef0ca74802a7fd82317d48a4a7448a5ea5018f6
Albin Joseph <albin.joseph@tryzens.com>
2021-08-11 22:28:27 +0530

    React server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const server = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.get('/', (req, res) => {
     res.send('Home Loaded');
